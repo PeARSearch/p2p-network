605496fa2e550209be67d3aa9afa7e7dca927a16
Nandaja Varma <nandaja.varma@gmail.com>
2016-09-25 23:48:05 +0530

    Adding the store value method to the DHT

--- create_network.py
+++ create_network.py
@@ -1,13 +1,32 @@
-import sys
+import sys, os
 import argparse
+import numpy
 import twisted.internet.reactor
 from entangled.node import EntangledNode
+from entangled.kademlia.datastore import SQLiteDataStore
+
+def storeValue(key, value, node):
+    """ Stores the specified value in the DHT using the specified key """
+    print '\nStoring value; Key: %s, Value: %s' % (key, value)
+    # Store the value in the DHT. This method returns a Twisted Deferred result, which we then add callbacks to
+    deferredResult = node.iterativeStore(key, value)
+    deferredResult.addErrback(genericErrorCallback)
+
+def genericErrorCallback(failure):
+    """ Callback function that is invoked if an error occurs during any of the DHT operations """
+    print 'An error has occurred:', failure.getErrorMessage()
+    twisted.internet.reactor.callLater(0, stop)
+
+def stop():
+    """ Stops the Twisted reactor, and thus the script """
+    print '\nStopping Kademlia node and terminating script...'
+    twisted.internet.reactor.stop()
 
 def main(args):
     arg = parse_arguments(args)
     port = arg.udp_port
     if arg.known_ip and arg.known_port:
-        known_nodes = [(arg.known_ip, int(arg.known_node))]
+        known_nodes = [(arg.known_ip, int(arg.known_port))]
     elif arg.config_file:
         known_nodes = []
         f = open(arg.config_file, 'r')
@@ -18,8 +37,20 @@ def main(args):
             known_nodes.append((ip_address, int(udp_port)))
     else:
         known_nodes = None
-    node = EntangledNode(udpPort=int(port))
+
+    # Set up SQLite-based data store
+    if os.path.isfile('/tmp/dbFile%s.db' % sys.argv[1]):
+        os.remove('/tmp/dbFile%s.db' % sys.argv[1])
+    data_store = SQLiteDataStore(dbFile = '/tmp/db_file_dht%s.db' % port)
+
+    # TODO: Get the pear profile from the PeARS instance using the TODO API
+    pear_profile = numpy.loadtxt('pear_profile.txt')
+    # TODO: Create Key and Value using LSH
+    KEY = "dummy"
+    VALUE = "dummy"
+    node = EntangledNode(udpPort=int(port), dataStore=data_store)
     node.joinNetwork(known_nodes)
+    twisted.internet.reactor.callLater(2.5, storeValue, KEY, VALUE, node)
     print "Starting the DHT node..."
     twisted.internet.reactor.run()
 
@@ -42,6 +73,11 @@ def parse_arguments(args=None):
         if not all([getattr(args,x) for x in required_together]):
             raise RuntimeError("Cannot supply Known Node IP without"\
                 " Known IP port")
+    else:
+        print "\nNOTE: You have not specified any remote DHT node(s) to connect to."
+        print "It will thus not be aware of any existing DHT, but will still "\
+                "function as a self-contained DHT (until another node "\
+                "contacts it).\n"
     return args
 
 if __name__ == '__main__':
